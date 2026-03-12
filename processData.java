7b091ee4d73c40978eb5b5e5fab32c6d4d9aa744
SUDANDHERAA S V <sudandheraasv@gmail.com>
2026-03-12 21:37:29 +0530

    processData method

---
+++ processData.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+public static void processData(int N, String S) {
+        System.out.println(N * 2);
+        System.out.println(S);
+    }
+    public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String S = sc.next();
+        processData(N, S);   
+    }
+}
